74d7bb049c53db76540ba7ef65e99f98da1cca96
yechielw <41305372+yechielw@users.noreply.github.com>
2020-11-22 21:12:30 +0200

    Update checkrunevery60s.ps1

--- checkrunevery60s.ps1
+++ checkrunevery60s.ps1
@@ -1,12 +1,11 @@
-$runStart = Get-itemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+$runStart = Get-itemProperty -path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run, HKCU:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run, "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows\Run", HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce, HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnceEx, HKCU:\Software\Microsoft\Windows\CurrentVersion\RunServices, HKCU:\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce, HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run, HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce, HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnceEx, HKLM:\System\CurrentControlSet\Services, HKLM:\Software\Microsoft\Windows\CurrentVersion\RunServices, HKLM:\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce -ErrorAction Ignore
 $runStart = $runStart.psObject.properties | Where-Object{$_.name -notlike 'PS*'} | Select-Object value
 $runStart = $runStart.ForEach({$_.value})
 $a = 0
 while($true){
     $a ++
     write-host -ForegroundColor blue "scan #"$a ", @" (get-date) ">>>"
-    Start-Sleep -Seconds 60
-    $tempRun = Get-itemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+    $tempRun = Get-itemProperty -path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run, HKCU:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run, "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows\Run", HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce, HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnceEx, HKCU:\Software\Microsoft\Windows\CurrentVersion\RunServices, HKCU:\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce, HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run, HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce, HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnceEx, HKLM:\System\CurrentControlSet\Services, HKLM:\Software\Microsoft\Windows\CurrentVersion\RunServices, HKLM:\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce -ErrorAction Ignore
     $tempRun = $tempRun.psObject.properties | Where-Object{$_.name -notlike 'PS*'} | Select-Object value
     $tempRun = $tempRun.ForEach({$_.value})
     
@@ -14,11 +13,11 @@ while($true){
 
     if($alert){
         write-host -ForegroundColor red "Alert! <<<>>> Alert!" (get-date) "Alert! <<<>>> Alert!" 
-        Write-host "Summary:"
-        Compare-Object -ReferenceObject $runStart -DifferenceObject $tempRun
+        Write-host "Summary:" $alert
         $runStart = $tempRun
     }
     else{
         write-host -ForegroundColor green "<<<>>> Clean <<<>>>"
     }
+    Start-Sleep -Seconds 5
 }
