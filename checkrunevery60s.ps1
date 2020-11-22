d99c79af63aeb567fe37174af05641c92e808a96
yechielw <41305372+yechielw@users.noreply.github.com>
2020-11-22 19:06:16 +0200

    Update checkrunevery60s.ps1

--- checkrunevery60s.ps1
+++ checkrunevery60s.ps1
@@ -1,13 +1,13 @@
-$runStart0 = Get-itemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
-$runStart = $runStart0.psObject.properties | Where-Object{$_.name -notlike 'PS*'} | Select-Object value
+$runStart = Get-itemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+$runStart = $runStart.psObject.properties | Where-Object{$_.name -notlike 'PS*'} | Select-Object value
 $runStart = $runStart.ForEach({$_.value})
 $a = 0
 while($true){
     $a ++
     write-host -ForegroundColor blue "scan #"$a ", @" (get-date) ">>>"
     Start-Sleep -Seconds 60
-    $tempRun0 = Get-itemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
-    $tempRun = $tempRun0.psObject.properties | Where-Object{$_.name -notlike 'PS*'} | Select-Object value
+    $tempRun = Get-itemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+    $tempRun = $tempRun.psObject.properties | Where-Object{$_.name -notlike 'PS*'} | Select-Object value
     $tempRun = $tempRun.ForEach({$_.value})
     
     $alert = Compare-Object -ReferenceObject $runStart -DifferenceObject $tempRun -PassThru
